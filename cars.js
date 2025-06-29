09a59a59e966f4e2b718026c23fb71c49167c291
Tenneb0 <emriklt@gmail.com>
2025-06-29 09:46:21 +0200

    Create cars.js

---
+++ cars.js
@@ -0,0 +1,28 @@
+window.dbCars = [
+  {
+    name: 'Amelia Molteberg',
+    img: 'https://img.vsco.co/357504/238016525/68039ef9908bbd683cf8eeed/vsco_041925.jpg',
+    desc: '07, Thor Heyerdahl, Studie'
+  },
+  {
+    name: 'Alexandra Henriksen',
+    img: 'https://img.vsco.co/93df26/255162028/683265a22e8e67dd7adc7216/vsco_052525.jpg',
+    desc: '08, Thor Heyerdahl, Studie'
+  },
+  {
+    name: 'Elona Rafuna',
+    img: 'https://img.vsco.co/16bdbc/195980052/67cf334836f12f54023a3c50/vsco_031025.jpg',
+    desc: '07, Thor Heyerdahl, Studie'
+  },
+  {
+    name: 'Selma Nordkvelde',
+    img: 'https://img.vsco.co/cdn-cgi/image/width=450,height=800/0c4250/274490394/677597fc74564d591087ab0a/vsco_010125.jpg',
+    desc: '07 Thor Heyerdahl, Studie'
+  },
+  {
+    name: 'Mille Hegerstrand Aarsten',
+    img: 'https://img.vsco.co/cdn-cgi/image/width=1600,height=902/449a29/141561468/6730adf70e68ed2c569bd519/vsco_111024.jpg',
+    desc: '07 Thor Heyerdahl, Studie.'
+  }
+  
+];
